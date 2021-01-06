408d547a7d03795cdf1f30f341d50fdb11e02b54
fluencysama <63216823+fluencysama@users.noreply.github.com>
2021-01-06 14:45:10 +0800

    Create ComputeArea.java

---
+++ 1/ComputeArea.java
@@ -0,0 +1,11 @@
+public class ComputeArea {
+    public static void main (String[] args) {
+        double radius;
+        double area;
+        
+        radius = 20;
+        area = radius * radius *java.lang.Math.PI;
+        System.out.println("The area for the circle of radius "
+                + radius + " is "+area);
+    }
+}
