71aef0368ca1a37ae0cbf67e09c4bbe28b5cc5db
fluencysama <63216823+fluencysama@users.noreply.github.com>
2021-01-07 11:29:46 +0800

    Create HexFormatException.java

---
+++ 9/HexFormatException.java
@@ -0,0 +1,14 @@
+public class HexFormatException extends NumberFormatException {
+	private String hex; // Hex exception value
+
+	/** Construct a HexFormatException object */
+	public HexFormatException(String hex) {
+		super(hex + " is not a hex string");
+		this.hex = hex;
+	} 
+
+	/** Return hex exception value */
+	public String getHex() {
+		return hex;
+	}
+}
