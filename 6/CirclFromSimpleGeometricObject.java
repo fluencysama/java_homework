d78b33a0a618672e9277d324967832116895fb97
fluencysama <63216823+fluencysama@users.noreply.github.com>
2021-01-07 11:04:39 +0800

    Create CirclFromSimpleGeometricObject.java

---
+++ 6/CirclFromSimpleGeometricObject.java
@@ -0,0 +1,51 @@
+public class CirclFromSimpleGeometricObject extends GeometricObject
+{
+    private double radius;
+    
+    public Circle4()
+    {
+        
+    }
+    
+    public Circle4(double radius)
+    {
+        this.radius = radius;
+    }
+    
+    public Circle4(double radius, String colour, boolean filled)
+    {
+        this.radius = radius;
+        setColour(colour);
+        setFilled(filled);
+    }
+    
+    public double getRadius()
+    {
+        return radius;
+    }
+    
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+    }
+    
+    public double getArea()
+    {
+        return radius * radius * Math.PI;
+    }
+    
+    public double getDiameter()
+    {
+        return 2 * radius;
+    }
+    
+    public double getPerimeter()
+    {
+        return 2 * radius * Math.PI;
+    }
+    
+    public void printCircle()
+    {
+        System.out.println("The circle is created " + getDateCreated() + " and the radius is " + radius);
+    }
+}
